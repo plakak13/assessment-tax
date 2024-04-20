db34d629d76df6b033f8439955ed882959f24d95
Plakak <s.prajak39@gmail.com>
2024-04-20 17:11:29 +0700

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
+
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+		<-sig
+		fmt.Println("shutting down the server")
+		os.Exit(0)
+	}()
 }
