ba158a0695d1a3b434e5929275a58c9ff510c81d
Plakak <s.prajak39@gmail.com>
2024-04-24 22:37:49 +0700

    add unit test update tax deduction and update update tax dedution function

--- admin/handler.go
+++ admin/handler.go
@@ -1,9 +1,12 @@
 package admin
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/plakak13/assessment-tax/postgres"
+	"github.com/plakak13/assessment-tax/tax"
 )
 
 type Handler struct {
@@ -11,6 +14,8 @@ type Handler struct {
 }
 
 type Storer interface {
+	UpdateTaxDeduction(s postgres.SettingTaxDeduction) sql.Result
+	TaxDeductionByType(allowanceTypes []string) ([]tax.TaxDeduction, error)
 }
 
 func New(db Storer) *Handler {
@@ -18,6 +23,28 @@ func New(db Storer) *Handler {
 }
 
 func (h *Handler) AdminHandler(c echo.Context) error {
+	payload := new(Setting)
 
-	return c.JSON(http.StatusOK, SettingResponse{})
+	err := c.Bind(payload)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	tRows, err := h.store.TaxDeductionByType([]string{"personal"})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	s := postgres.SettingTaxDeduction{
+		ID:     tRows[0].ID,
+		Amount: payload.Amount,
+	}
+
+	row := h.store.UpdateTaxDeduction(s)
+	_, err = row.RowsAffected()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, SettingResponse{PersonalDeduction: payload.Amount})
 }
